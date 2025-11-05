fd485aeec296454baaf639785a98f826fe2e99b8
tuonidugou <hht@g.ecc.u-tokyo.ac.jp>
2025-11-05 21:06:50 +0900

    shape flag

--- shape.h
+++ shape.h
@@ -28,7 +28,7 @@ typedef uint32_t redblack_id_t;
 # define SHAPE_MASK (((uintptr_t)1 << SHAPE_ID_NUM_BITS) - 1)
 # define SHAPE_FLAG_MASK (((VALUE)-1) >> SHAPE_ID_NUM_BITS)
 
-# define SHAPE_FLAG_SHIFT ((SIZEOF_VALUE * 8) - SHAPE_ID_NUM_BITS)
+# define SHAPE_FLAG_SHIFT ((8 * 8) - SHAPE_ID_NUM_BITS)
 
 # define SHAPE_MAX_VARIATIONS 8
 
